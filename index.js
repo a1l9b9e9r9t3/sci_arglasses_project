c8fb74d23d6034d762ae4fcedc7d67d2fd7dfc2e
a1l9b9e9r9t3 <41937184+a1l9b9e9r9t3@users.noreply.github.com>
2018-08-09 10:34:17 +0900

    Update index.js

--- index.js
+++ index.js
@@ -133,6 +133,10 @@ app.post('/', function (request, response) {
             let responseToUser = { fulfillmentText: obj[object][SE][PE]};
             sendResponse(responseToUser);
         },
+        'object.who': () => {
+            let responseToUser = { fulfillmentText: obj[object][who]};
+            sendResponse(responseToUser);
+        },
         
         'default': () => {
             let responseToUser = { fulfillmentText: '죄송합니다. 정보가 없는 내용입니다. 다른 궁금한건 없으신가요?' };
